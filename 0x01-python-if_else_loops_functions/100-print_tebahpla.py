6379db1526270730a8e67dd1e5f328750c74bbf5
Tyrone Joel <tyronejoel04@gmail.com>
2023-08-09 16:38:24 -0700

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # 100-print_tebahpla.py
-# Print the reversed alphabet with alternating uppercase and lowercase characters.
+# Print the reversed alphabet with uppercase and lowercase characters.
 # Initialize a variable `i` to control the alternating case.
-# Loop through the ASCII values of lowercase letters from 'z' to 'a' in reverse order.
+# Loop through lowercase letters from 'z' to 'a' in reverse order.
 i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
-            print("{}".format(chr(c - i)), end="")
-            i = 32 if i == 0 else 0
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
