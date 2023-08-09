74b5d212f7ed1fe3154c23b1f7add04c1d7b5001
Tyrone Joel <tyronejoel04@gmail.com>
2023-08-09 16:58:58 -0700

    Remove at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 # 101-remove_char_at.py
 
-def remove_char_at(s, n):
-    """    
-    Create a new string from the input string.
-    :@ s: The input string from which the character should be removed.
-    :@ n: The index of the character to be removed.
-    :return: A new string with the specified character removed.
-    """
-    if n < 0 or n >= len(s):
-        return s[:n] + s[n + 1:]
+def remove_char_at(str, n):
+    if n < 0 or n >= len(str):
+        return (str)
+    return (str[:n] + str[n + 1:])
