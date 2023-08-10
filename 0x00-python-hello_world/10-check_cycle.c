1683ffbdb608e489927dc11161cf63de50bcd198
Tyrone Joel <tyronejoel04@gmail.com>
2023-08-09 17:18:27 -0700

    Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,16 +5,9 @@
 /**
  * check_cycle - checks if a linked list contains a cycle
  * @list: linked list to check
- * 
- * Return: 1 if the list has a cycle, 0 if it doesn't
+ *
+ * Return: 1 if the list has a cycle, 0 if it does not
  */
-
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-} listint_t;
-
 int check_cycle(listint_t *list)
 {
 	listint_t *slow = list, *fast = list;
