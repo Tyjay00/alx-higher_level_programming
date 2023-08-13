3b0de430cd22be89080a48466e5cb7ddcbd86279
Tyrone Joel <tyronejoel04@gmail.com>
2023-08-13 05:09:43 -0700

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    """Display the addition, subtraction, multiplication, and division results of values a and b."""
+    from calculator_1 import add, sub, mul, div
 
-def perform_calculations(a, b):
-    """Print the sum, difference, multiple and quotient of a and b."""
+    a = 10
+    b = 5
+
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
-
-    if __name__ == "__main__":
-        a = 10
-        b = 5
-    perform_calculations(a, b)
