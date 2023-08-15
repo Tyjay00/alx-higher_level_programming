8d5b7bc55030a2060dc18725d5d1e3a649c3e524
Tyrone Joel <tyronejoel04@gmail.com>
2023-08-15 03:53:32 -0700

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# The function checks whether the index is valid 
+# The function checks whether the index is valid
 
 def new_in_list(my_list, idx, element):
     """Replace an element in a list at a specific position."""
