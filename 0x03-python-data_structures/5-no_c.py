02318e5c996e55b9116831515a7d32ebbf83ca81
Tyrone Joel <tyronejoel04@gmail.com>
2023-08-14 17:37:01 -0700

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# Removing the specified characters from the input string.
 
 def no_c(my_string):
     """Remove all characters c and C from a string."""
-    new_string = ''.join([char for char in my_string if char.lower() != 'c'])
-    return new_string
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return "".join(copy)
 
