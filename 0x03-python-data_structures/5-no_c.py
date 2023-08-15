7f3c4e6f3991d100b8e4fd2f8671a6191fe396a3
Tyrone Joel <tyronejoel04@gmail.com>
2023-08-14 17:51:31 -0700

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# Remove all characters c and C from a string
-def no_c(my_string):
-    new_str = '' 
-    for i in my_string:
-        if i != 'C' and i != 'c':
-            new_str += i 
 
-    return new_str
+def no_c(my_string):
+    new_string = ""
+    for elements in my_string:
+        if elements != "c" and elements != "C":
+            new_string += elements
+    return new_string
+
