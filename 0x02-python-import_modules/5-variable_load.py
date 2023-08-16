53cd8ec9399ed1fbc41c7b300138bb587e0515a7
Tyrone Joel <tyronejoel04@gmail.com>
2023-08-15 18:55:35 -0700

    Everything can be imported

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Prints the value of the variable "a".
+
+if __name__ == "__main__":
+    import variable_load_5
+
+    value_of_a = variable_load_5.a
+    print("Value of variable 'a':", value_of_a)
