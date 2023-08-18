d0f44801844fb7cc6e15c1a1f2b512354c5661ee
Tyrone Joel <tyronejoel04@gmail.com>
2023-08-18 11:53:31 -0700

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-#calculates the square matrix using lambda functions and the map.
 
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
-
+    """calculates the square matrix using lambda functions and the map"""
     for i in range(len(matrix)):
         new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
