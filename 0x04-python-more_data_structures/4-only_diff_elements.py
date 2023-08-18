cfb82978fe8f85361a97b07683d9db8cf4bff793
Tyrone Joel <tyronejoel04@gmail.com>
2023-08-18 13:25:22 -0700

    Update dictionary

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def only_diff_elements(set_1, set_2):
         diff_set = (set_1 - set_2) | (set_2 - set_1)
         set_1 = {1, 2, 3, 4, 5}
--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
-        a_dictionary[key] = value
-    return a_dictionary
+    a_dictionary[key] = value
+    return (a_dictionary)
