339b0c2a39843160a418a822f523e33ebe5a512e
Tyrone Joel <tyronejoel04@gmail.com>
2023-09-05 18:46:17 -0700

    Python - Test-driven development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-add_integer = __import__('0-add_integer').add_integer
+# 0-add_integer.py
 
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
+"""adds 2 integers."""
 
 
+def add_integer(a, b=98):
+    """Return the int: The addition of a and b.
+
+    Float arguments are casted using the int function
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
