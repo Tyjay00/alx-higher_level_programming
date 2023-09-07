6c98f7b612c767257ca2bc63736188483f372538
Tyrone Joel <tyronejoel04@gmail.com>
2023-09-07 08:39:10 -0700

    Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,164 +1,59 @@
 #!/usr/bin/python3
-
-"""
-This Python module defines a simple Rectangle class with width and height attributes.
-
-"""
-
+"""Defines a Rectangle class."""
 
 class Rectangle:
-    """
-
-    This class represents a rectangular shape with width and height attributes.
-
-    """
+    """Represents a simple rectangle with width and height attributes."""
 
     def __init__(self, width=0, height=0):
-        """
-
-        Initializes a Rectangle object with optional width and height parameters.
+        """Initialize a new Rectangle.
 
         Args:
-            width (int, optional): The width of the Rectangle.
-            height (int, optional): The height of the Rectangle.
-
+            width (int, optional): The width of the rectangle.
+            height (int, optional): The height of the rectangle.
         """
-
-        self.__check_valid_width(width)
-        self.__check_valid_height(height)
-
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """
-
-        Get the width of the Rectangle.
-
-        """
-
+        """Get or set the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-
-        Set the width of the Rectangle and validate the input.
+        """Set the width of the rectangle and validate the input.
 
         Args:
-            value (int): The width of the Rectangle.
+            value (int): The width of the rectangle.
 
         Raises:
-            TypeError: If `value` type is not integer.
+            TypeError: If `value` is not an integer.
             ValueError: If `value` is less than 0.
-
         """
-
-        self.__check_valid_width(value)
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """
-
-        Get the height of the Rectangle.
-
-        """
-
+        """Get or set the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-
-        Set the height of the Rectangle and validate the input.
+        """Set the height of the rectangle and validate the input.
 
         Args:
-            value (int): The height of the Rectangle.
+            value (int): The height of the rectangle.
 
         Raises:
-            TypeError: If `value` type is not integer.
+            TypeError: If `value` is not an integer.
             ValueError: If `value` is less than 0.
-
         """
-
-        self.__check_valid_height(value)
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
-
-    def __check_valid_width(self, width):
-        """
-
-        Check if the width is a valid integer.
-
-        Args:
-            width (int): The width of the Rectangle.
-
-        Raises:
-            TypeError: If `width` type is not integer.
-            ValueError: If `width` is less than 0.
-
-        """
-
-        if self.__check_int_value(width) is False:
-            raise TypeError('width must be an integer')
-
-        if self.__check_positive_value(width) is False:
-            raise ValueError('width must be >= 0')
-
-    def __check_valid_height(self, height):
-        """
-
-        Checks if the height is a valid integer
-
-        Args:
-            height (int): The height of the Rectangle.
-
-        Raises:
-            TypeError: If `height` type is not integer.
-            ValueError: If `height` is less than 0.
-
-        """
-
-        if self.__check_int_value(height) is False:
-            raise TypeError('height must be an integer')
-
-        if self.__check_positive_value(height) is False:
-            raise ValueError('height must be >= 0')
-
-    def __check_int_value(self, value):
-        """
-
-        Checks if the value is an integer
-
-        Args:
-            value (int): The number to verify
-
-        Returns:
-            int: If is a int True, False otherwise.
-
-        """
-
-        if type(value) is int:
-            return True
-
-        return False
-
-    def __check_positive_value(self, value):
-        """
-
-        Checks if the value is a positive integer
-
-        Args:
-            value (int): The number to verify
-
-        Returns:
-            int: True If value is greater than
-            or equal to 0, False otherwise.
-
-        """
-
-        if value >= 0:
-            return True
-
-        return False
