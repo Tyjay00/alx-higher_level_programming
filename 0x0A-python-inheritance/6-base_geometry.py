cbf2fd2b5662ee9fc79574493c6082c8824e6bca
Tyrone Joel <tyronejoel04@gmail.com>
2023-09-11 15:09:57 -0700

    Improve Geometry

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
 """Defines a base geometry class BaseGeometry."""
 
-
 class BaseGeometry:
-    """
-    A base class for geometry-related operations.
-
-    This class serves as a foundation for geometry classes and includes a placeholder method, `area()`,
-    which raises an exception to indicate that it is not implemented yet.
-    """
+    """A base class for geometry-related operations."""
 
     def area(self):
-        """
-        Calculate the area of the geometric shape.
-
-        This method is intended to be overridden by subclasses with specific geometric shapes.
-        """
+        """Calculate the area of the geometric shape."""
         raise Exception("area() is not implemented")
