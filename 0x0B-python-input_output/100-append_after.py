978c2d66af92d1c0bca3f44cf491970de1604544
Tyrone Joel <tyronejoel04@gmail.com>
2023-09-13 15:06:49 -0700

    Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a function to insert text in a text file."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert text after each line containing a given string in a file."""
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
