736b69205581bc1ec4a52d8494ea00aff50ca5ee
Tyrone Joel <tyronejoel04@gmail.com>
2023-11-27 15:45:19 -0800

    Object-relational mapping

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""This module contains the class definition of a State"""
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
+
 class State(Base):
-"""This class defines each state. It inherits from the Base class."""
+    """
+    Class with id and name attributes of each state
+    """
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states")
