0e941483bb288094c464f626ad34e1196932f07e
Tyrone Joel <tyronejoel04@gmail.com>
2023-12-05 06:37:45 -0800

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-"""Finds peak in unsorted integers"""
+"""Finds a peak in a list of unsorted integers"""
 
-def find_peak(lst):
-    """Finds a peak in lst"""
 
-    if lst is None or lst == []:
+def find_peak(list_of_integers):
+    """Finds a peak in list_of_integers"""
+
+    if list_of_integers is None or list_of_integers == []:
         return None
     lo = 0
-    hi = len(lst)
+    hi = len(list_of_integers)
     mid = ((hi - lo) // 2) + lo
     mid = int(mid)
     if hi == 1:
-        return lst[0]
+        return list_of_integers[0]
     if hi == 2:
-        return max(lst)
-    if lst[mid] >= lst[mid - 1] and lst[mid] >= lst[mid + 1]:
-        return lst[mid]
-    if mid > 0 and lst[mid] < lst[mid + 1]:
-        return find_peak(lst[mid:])
-    if mid > 0 and lst[mid] < lst[mid - 1]:
-        return find_peak(lst[:mid])
-
-if __name__ == "__main__":
-    with open("6-peak.txt", "r") as file:
-        lines = file.readlines()
-        num_lines = len(lines)
-
-    peak_result = find_peak([int(line.strip()) for line in lines]) if num_lines > 0 else None
-
-    print(f"{num_lines} 6-peak.txt")
-    print(f"Peak Element: {peak_result}")
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
