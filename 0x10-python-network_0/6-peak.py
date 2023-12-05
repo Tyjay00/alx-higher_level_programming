0b9a3d53df3214488b93b66eae6f0a04e8969f41
Tyrone Joel <tyronejoel04@gmail.com>
2023-12-05 05:54:06 -0800

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -20,3 +20,13 @@ def find_peak(lst):
         return find_peak(lst[mid:])
     if mid > 0 and lst[mid] < lst[mid - 1]:
         return find_peak(lst[:mid])
+
+if __name__ == "__main__":
+    with open("6-peak.txt", "r") as file:
+        lines = file.readlines()
+        num_lines = len(lines)
+
+    peak_result = find_peak([int(line.strip()) for line in lines]) if num_lines > 0 else None
+
+    print(f"{num_lines} 6-peak.txt")
+    print(f"Peak Element: {peak_result}")
