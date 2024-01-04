18e2d305e8a28c7d574431e31d805cdb47b60a52
Tyrone Joel <tyronejoel04@gmail.com>
2024-01-03 17:18:51 -0800

    Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-//This is a Node.js script that downloads a file from a given URL and saves it to a specified location.
-
+// downloads a file from a given URL and saves it to a location.
 const fs = require('fs');
 const request = require('request');
 request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
