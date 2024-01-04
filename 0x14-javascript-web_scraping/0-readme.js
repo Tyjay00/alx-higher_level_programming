95a5b259f0a11ef17d723a26ee16382d5db61013
Tyrone Joel <tyronejoel04@gmail.com>
2024-01-03 17:10:42 -0800

    Write me

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-//This is a Node.js script that reads a file and prints its content to the console.
-
-#!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
   console.log(error || content);
